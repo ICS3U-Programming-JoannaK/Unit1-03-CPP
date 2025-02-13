13f2a09fd3e309792976fd60cd4bbbdbab2a43f6
Joanna Keza <joanna.keza@stu.ocsb.ca>
2025-02-13 08:29:06 +0000

    Added

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2025 Joanna Keza All rights reserved.
+//
+// Created by: Joanna Keza
+// Date: February 12, 2025
+// This program displays "Hello, World! with style".
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World! with style";
+}
